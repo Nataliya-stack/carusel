4c9232aa512f4b2fdffef3bd72dfd632d6218d57
Natalia Yanovich <nataliya.yanovic@gmail.com>
2026-07-14 13:55:38 +0200

    tailwing no es functiona. corregimos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://github.io',
+  site: 'https://github.io',  
   vite: {
     plugins: [tailwindcss()]
   }
