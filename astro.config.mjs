d2d3aee92fdcdbd4a4a90afccd12efc37a519c40
Natalia Yanovich <nataliya.yanovic@gmail.com>
2026-07-14 17:35:07 +0200

    настроили tailwind на git hub

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://nataliya-stack.github.io/', 
+  site: 'https://github.io', 
   base: '/carusel/', 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          // Отключаем случайные хэши для CSS файлов при сборке!
+          assetFileNames: 'assets/[name][extname]', 
+        }
+      }
+    }
   }
 });
+
+
